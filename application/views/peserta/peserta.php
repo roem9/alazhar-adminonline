1c2b0fa06fe36310ecddc8105c80c30b4e00fef9
roem9 <muhammadrum04@gmail.com>
2021-01-29 17:58:46 +0700

    goo

--- application/views/peserta/peserta.php
+++ application/views/peserta/peserta.php
@@ -187,7 +187,7 @@
                                             <option value="<?= $data['id_kelas']?>"><?= $data['nama_kelas']?></option>
                                         <?php endforeach;?>
                                     </select>
-                                    <small id="emailHelp" class="form-text text-danger">*apabila kelas dikosongkan maka akan masuk waiting list</small>
+                                    <small id="emailHelp" class="form-text text-danger">*apabila kelas dikosongkan maka peserta akan masuk waiting list</small>
                                 </div>
                                 <div class="form-group">
                                     <label for="biaya">Biaya</label>
